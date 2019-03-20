6cd2e8a32db594ee214fe438770f3852b631ffcf
Alex <7259606@mail.ru>
2019-03-20 16:38:37 +0300

    add photo in sait

--- src/store/modules/albums.js
+++ src/store/modules/albums.js
@@ -2,13 +2,13 @@ export default {
     namespaced: true,
     state: {
         albumsList: [
-            {id: 0, title: 'Краткое название альбома 1', previewPhoto: './static/img/photo/photo1.jpg'},
-            {id: 1, title: 'Краткое название альбома 2', previewPhoto: './static/img/photo/photo1.jpg'},
-            {id: 2, title: 'Краткое название альбома 3', previewPhoto: './static/img/photo/photo1.jpg'},
-            {id: 3, title: 'Краткое название альбома 4', previewPhoto: './static/img/photo/photo1.jpg'},
-            {id: 4, title: 'Краткое название альбома 4', previewPhoto: './static/img/photo/photo1.jpg'},
-            {id: 5, title: 'Краткое название альбома 4', previewPhoto: './static/img/photo/photo1.jpg'},
-            {id: 6, title: 'Краткое название альбома 4', previewPhoto: './static/img/photo/photo1.jpg'}
+            {id: 0, title: 'Краткое название альбома 1', previewPhoto: './images/photo1.jpeg'},
+            {id: 1, title: 'Краткое название альбома 2', previewPhoto: './images/photo1.jpeg'},
+            {id: 2, title: 'Краткое название альбома 3', previewPhoto: './images/photo1.jpeg'},
+            {id: 3, title: 'Краткое название альбома 4', previewPhoto: './images/photo1.jpeg'},
+            {id: 4, title: 'Краткое название альбома 4', previewPhoto: './images/photo1.jpeg'},
+            {id: 5, title: 'Краткое название альбома 4', previewPhoto: './images/photo1.jpeg'},
+            {id: 6, title: 'Краткое название альбома 4', previewPhoto: './images/photo1.jpeg'}
         ]
     },
     getters: {
--- src/store/modules/trends.js
+++ src/store/modules/trends.js
@@ -2,12 +2,12 @@ export default {
     namespaced: true,
     state: {
         trends: [
-            {description: 'Описание Бачата',icon: './static/img/dance1.png',text:'Бачата', url:'bachata'},
-            {description: 'Описание Сальса',icon: './static/img/dance2.png',text:'Сальса', url:'salsa'},
-            {description: 'Описание Танго',icon: './static/img/dance3.png',text:'Танго', url:'tango'},
-            {description: 'Описание Диско',icon: './static/img/dance1.png',text:'Дискофокс', url:'disco'},
-            {description: 'Описание Меренга',icon: './static/img/dance2.png',text:'Меренга', url:'merenga'},
-            {description: 'Описание Всех направлений',icon: './static/img/dance3.png',text:'Групповые номера', url:'all'},
+            {description: 'Описание Бачата',icon: './images/dance1.png',text:'Бачата', url:'bachata'},
+            {description: 'Описание Сальса',icon: './images/dance2.png',text:'Сальса', url:'salsa'},
+            {description: 'Описание Танго',icon: './images/dance3.png',text:'Танго', url:'tango'},
+            {description: 'Описание Диско',icon: './images/dance1.png',text:'Дискофокс', url:'disco'},
+            {description: 'Описание Меренга',icon: './images/dance2.png',text:'Меренга', url:'merenga'},
+            {description: 'Описание Всех направлений',icon: './images/dance3.png',text:'Групповые номера', url:'all'},
         ],
     },
     getters: {
