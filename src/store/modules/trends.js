63affffc6254268eaa725c67f6fc1e4b5923e887
Alex <7259606@mail.ru>
2019-03-20 14:55:52 +0300

    fix photo bugs

--- src/store/modules/trends.js
+++ src/store/modules/trends.js
@@ -2,12 +2,12 @@ export default {
     namespaced: true,
     state: {
         trends: [
-            {description: 'Описание Бачата',icon: '../img/dance1.png',text:'Бачата', url:'bachata'},
-            {description: 'Описание Сальса',icon: '../img/dance2.png',text:'Сальса', url:'salsa'},
-            {description: 'Описание Танго',icon: '../img/dance3.png',text:'Танго', url:'tango'},
-            {description: 'Описание Диско',icon: '../img/dance1.png',text:'Дискофокс', url:'disco'},
-            {description: 'Описание Меренга',icon: '../img/dance2.png',text:'Меренга', url:'merenga'},
-            {description: 'Описание Всех направлений',icon: '../img/dance3.png',text:'Групповые номера', url:'all'},
+            {description: 'Описание Бачата',icon: './img/dance1.png',text:'Бачата', url:'bachata'},
+            {description: 'Описание Сальса',icon: './img/dance2.png',text:'Сальса', url:'salsa'},
+            {description: 'Описание Танго',icon: './img/dance3.png',text:'Танго', url:'tango'},
+            {description: 'Описание Диско',icon: './img/dance1.png',text:'Дискофокс', url:'disco'},
+            {description: 'Описание Меренга',icon: './img/dance2.png',text:'Меренга', url:'merenga'},
+            {description: 'Описание Всех направлений',icon: './img/dance3.png',text:'Групповые номера', url:'all'},
         ],
     },
     getters: {
